31ab78e21c5d2acc01d8bffcc6da108b6842fc8c
Your Name <e@w.com>
2022-02-17 10:38:38 +0300

    v 2.0

--- homework.py
+++ homework.py
@@ -1,26 +1,25 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
 class InfoMessage:
     """(RU) Информационное сообщение о тренировке.
        (EN) Informational message about the training."""
 
-    def __init__(self,
-                 training_type: str,
-                 duration: int,
-                 distance: float,
-                 speed: float,
-                 calories: float
-                 ) -> None:
-        self.training_type: str = training_type
-        self.duration: int = duration
-        self.distance: float = distance
-        self.speed: float = speed
-        self.calories: float = calories
+    training_type: str
+    duration: int
+    distance: float
+    speed: float
+    calories: float
 
     def get_message(self) -> str:
-        return (f'Тип тренировки: {self.training_type}; '
-                f'Длительность: {self.duration:.3f} ч.; '
-                f'Дистанция: {self.distance:.3f} км; '
-                f'Ср. скорость: {self.speed:.3f} км/ч; '
-                f'Потрачено ккал: {self.calories:.3f}.')
+        message = (f'Тип тренировки: {self.training_type}; '
+                   f'Длительность: {self.duration:.3f} ч.; '
+                   f'Дистанция: {self.distance:.3f} км; '
+                   f'Ср. скорость: {self.speed:.3f} км/ч; '
+                   f'Потрачено ккал: {self.calories:.3f}.')
+        return message
 
 
 class Training:
@@ -28,9 +27,9 @@ class Training:
 
     M_IN_KM: float = 1000.0
     LEN_STEP: float = 0.65
-    VMIN: int = 60
+    VMIN: float = 60.0
 
-    def __init__(self, action, duration, weight):
+    def __init__(self, action: int, duration: int, weight: float) -> None:
         self.action: int = action
         self.duration: int = duration
         self.weight: float = weight
@@ -48,7 +47,7 @@ class Training:
     def get_spent_calories(self) -> float:
         """(RU) Получить количество затраченных калорий.
            (EN) Get the number of calories consumed."""
-        pass
+        raise NotImplementedError()
 
     def show_training_info(self):
         """(RU) Вернуть информационное сообщение о выполненной тренировке.
@@ -63,26 +62,20 @@ class Training:
 class Running(Training):
     """(RU)Тренировка: бег.
        (EN) Training: running."""
-    CONS_CAL1 = 18
-    CONS_CAL2 = 20
-
-    def __init__(self,
-                 action: int,
-                 duration: int,
-                 weight: float,
-                 ) -> None:
-        super().__init__(action, duration, weight)
+    CALL_SPD_MULTIPIER1 = 18
+    CALL_SPD_MULTIPIER2 = 20
 
     def get_spent_calories(self) -> float:
-        return ((self.CONS_CAL1 * self.get_mean_speed() - self.CONS_CAL2)
-                * self.weight / self.M_IN_KM * (self.duration * self.VMIN))
+        return ((self.CALL_SPD_MULTIPIER1 * self.get_mean_speed()
+                - self.CALL_SPD_MULTIPIER2) * self.weight
+                / self.M_IN_KM * (self.duration * self.VMIN))
 
 
 class SportsWalking(Training):
     """(RU) Тренировка: спортивная ходьба.
        (EN) Training: sports walking."""
-    CAL3 = 0.035
-    CAL4 = 0.029
+    CALL_SPD_MULTIPIER3 = 0.035
+    CALL_SPD_MULTIPIER4 = 0.029
 
     def __init__(self, action: int, duration: int, weight: float,
                  height: float) -> None:
@@ -90,14 +83,17 @@ class SportsWalking(Training):
         self.height = height
 
     def get_spent_calories(self) -> float:
-        return ((self.CAL3 * self.weight
-                 + (self.get_mean_speed() ** 2 // self.height)
-                 * self.CAL4 * self.weight) * (self.duration * self.VMIN))
+        return ((self.CALL_SPD_MULTIPIER3 * self.weight
+                + (self.get_mean_speed() ** 2 // self.height)
+                * self.CALL_SPD_MULTIPIER4 * self.weight)
+                * (self.duration * self.VMIN))
 
 
 class Swimming(Training):
     """(RU) Тренировка: плавание."""
     LEN_STEP: float = 1.38
+    CALL_SPD_MULTIPLIER5 = 1.1
+    CALL_SPD_MULTIPLIER6 = 2
 
     def __init__(self,
                  action: int,
@@ -106,34 +102,38 @@ class Swimming(Training):
                  length_pool: float,
                  count_pool: int) -> None:
         super().__init__(action, duration, weight)
-        self.length_pool: float = length_pool
-        self.count_pool: int = count_pool
+        self.length_pool = length_pool
+        self.count_pool = count_pool
 
     def get_mean_speed(self) -> float:
         return (self.length_pool * self.count_pool
                 / self.M_IN_KM / self.duration)
 
     def get_spent_calories(self) -> float:
-        const_k = 1.1
-        return (self.get_mean_speed() + const_k) * 2 * (self.weight)
+        return ((self.get_mean_speed() + self.CALL_SPD_MULTIPLIER5)
+                * self.CALL_SPD_MULTIPLIER6 * (self.weight))
 
 
-def read_package(workout_type: str, data: list) -> Training:
+def read_package(workout_type: str, data: List[float]) -> Training:
     """(RU) Прочитать данные полученные от датчиков.
        (EN) Read the data received from the sensors."""
-    read1: dict[str, Training] = {
+    training_types: dict[str, Training] = {
         'RUN': Running,
         'WLK': SportsWalking,
         'SWM': Swimming
     }
 
-    return read1[workout_type](*data)
+    try:
+        if workout_type in training_types:
+            return training_types[workout_type](*data)
+    except KeyError:
+        print('Неверный тип тренировки')
 
 
 def main(training: Training) -> str:
     """(RU) Главная функция.
        (EN) The main function."""
-    info = InfoMessage.get_message(training.show_training_info())
+    info: InfoMessage = InfoMessage.get_message(training.show_training_info())
     print(info)
 
 
@@ -146,7 +146,4 @@ if __name__ == '__main__':
 
     for workout_type, data in packages:
         training: Training = read_package(workout_type, data)
-        if training is None:
-            print('Неожиданный тип тренировки')
-        else:
-            main(training)
+        main(training)
